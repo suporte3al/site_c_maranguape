19b74d76af020a287a23fe50bc9bd831ca6a6d1a
suporte atacado <suporte3al@gmail.com>
2023-03-28 11:30:18 -0300

    alteração no diretorio das marcas proprias e no header

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,9 @@ document.addEventListener('scroll', () => {
   var scroll_position = window.scrollY;
   if (scroll_position > 250) {
     header.style.backgroundColor = '#2D4689';
+    header.style.borderBottom = 'solid 1px #F44336';
   } else {
     header.style.backgroundColor = 'transparent';
+    header.style.borderBottom = 'none';
   }
 });
