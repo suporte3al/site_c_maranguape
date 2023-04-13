c80ce84e5bc83532c9819d770c9181f9ac60f079
suporte atacado <suporte3al@gmail.com>
2023-04-13 15:27:08 -0300

    modificando o header e funcionalidades do footer

--- src/js/main.js
+++ src/js/main.js
@@ -4,10 +4,11 @@ const header = document.querySelector('.header');
 document.addEventListener('scroll', () => {
   var scroll_position = window.scrollY;
   if (scroll_position > 250) {
-    header.style.backgroundColor = '#2D4689';
-    header.style.borderBottom = 'solid 1px #F44336';
+    header.style.borderBottom = 'solid 1px #000';
+    header.style.background = 'linear-gradient(180deg, #2D4689 0%, rgba(45, 70, 137, 0.8) 100%)';
   } else {
     header.style.backgroundColor = 'transparent';
     header.style.borderBottom = 'none';
+    header.style.background = 'none';
   }
 });
