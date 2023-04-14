2bdf6d17ad2519d84f3786e2a60cbddcf2188ce9
suporte atacado <suporte3al@gmail.com>
2023-04-14 14:57:22 -0300

    atualizando o design da home

--- src/js/main.js
+++ src/js/main.js
@@ -4,8 +4,8 @@ const header = document.querySelector('.header');
 document.addEventListener('scroll', () => {
   var scroll_position = window.scrollY;
   if (scroll_position > 250) {
-    header.style.borderBottom = 'solid 1px #000';
-    header.style.background = 'linear-gradient(180deg, #2D4689 0%, rgba(45, 70, 137, 0.8) 100%)';
+    header.style.borderBottom = 'solid 3px #2D4689';
+    header.style.background = 'linear-gradient(180deg, #2D4689 35%, rgba(45, 70, 137, 0.8) 100%)';
   } else {
     header.style.backgroundColor = 'transparent';
     header.style.borderBottom = 'none';
